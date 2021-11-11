bf0e192d880f55bfcd4f383df2b827ddb2c900db
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-11 18:45:17 -0300

    Requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -36,7 +36,7 @@ console.log (concatName (['captain', 'my', 'captain']))
 
 // Desafio 5 - OK
 function footballPoints(wins, ties) {
-  
+
   let pontosVitorias = wins * 3;
   let pontos = pontosVitorias + ties;
 
@@ -44,19 +44,46 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+  
 }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
+// Desafio 7 - OK
+function catAndMouse(mouse, cat1, cat2) {
+
+  let distCat1 = mouse - cat1;
+  let distCat2 = mouse - cat2;
+
+  let gato1 = Math.abs(distCat1);
+  let gato2 = Math.abs (distCat2);
+
+  if (gato1 < gato2){
+    return 'cat1';
+  } else if (gato2 < gato1){
+    return 'cat2';
+  } else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// Desafio 8 - OK
+function fizzBuzz(numeros) {
+  let novoBuzz = [];
+  
+  for (let i in numeros)
+    if (numeros[i] % 3 === 0 && numeros[i] % 5 === 0){
+      novoBuzz.push ('fizzBuzz');
+    } else if (numeros[i] % 5 === 0) {
+      novoBuzz.push ('buzz');
+    } else if (numeros[i] % 3 === 0) {
+        novoBuzz.push ('fizz');
+    } else {
+      novoBuzz.push ('bug!')
+    }
+
+    return novoBuzz
+  }
+
 
 // Desafio 9
 function encode() {
