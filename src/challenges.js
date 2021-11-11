8b2baf02905a7913e5d9aee81760a8b915b810e6
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-11 15:05:20 -0300

    Requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(par1, par2) {
+  
+  if (par1 === 'true' || par1 === 'True' && par2 === 'true' || par2 === 'True'){
+    return 'true'
+  } else {
+    return 'false'
+  }
 }
 
 // Desafio 2
